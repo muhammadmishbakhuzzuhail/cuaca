555fe1beecf4a4b9ec042a27e97dc70768aaae67
Muhammad Mishbakhuz Zuhail <muhammadmishbakhuzzuhail@gmail.com>
2025-11-18 01:04:20 +0700

    final-3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,17 @@ import SearchDropdown from "./_components/search-dropdown";
 import SearchInput from "./_components/search-input";
 
 const Home = () => {
-   return (
-      <div className="space-y-4 ">
-         <div className="mx-auto w-fit space-y-2">
-            <div className="flex w-fit mx-auto">
-               <ButtonSearchDropdown />
-               <SearchInput />
-            </div>
-            <SearchDropdown />
-         </div>
-         <GridCardPreview />
+  return (
+    <div className="space-y-4 ">
+      <div className="mx-auto w-fit space-y-2">
+        <div className="flex w-fit mx-auto">
+          <ButtonSearchDropdown />
+          <SearchInput />
+        </div>
+        <SearchDropdown />
       </div>
-   );
+      <GridCardPreview />
+    </div>
+  );
 };
 export default Home;
